a139e23040143370273e5a1e8dfbc246174b0030
hjshin <>
2025-07-24 13:20:21 +0900

    MenuBar에 MacOS System8 테마 항목 추가: 사용자가 MacOS System8 테마를 선택할 수 있도록 MenuBar에 새로운 항목을 추가하였습니다. 이를 통해 사용자 선택의 폭을 더욱 넓히고, 다양한 테마 옵션을 제공합니다.

--- components/MenuBar.tsx
+++ components/MenuBar.tsx
@@ -60,9 +60,9 @@ const MenuBar: React.FC<MenuBarProps> = ({ onOpenHelp, onLogout, user }) => {
       name: '테마', 
       items: [
         { label: 'Win11', action: () => window.open('https://win11-board.vercel.app/', '_blank', 'noopener,noreferrer'), disabled: false },
+        { label: 'Ubuntu', action: () => window.open('https://ubuntu-board.vercel.app/', '_blank', 'noopener,noreferrer'), disabled: false },
         { label: 'WinXP', action: () => window.open('https://winxp-board.vercel.app/', '_blank', 'noopener,noreferrer'), disabled: false },
-        { label: 'Ubuntu', action: () => window.open('https://ubuntu-board.vercel.app/', '_blank', 'noopener,noreferrer'), disabled: false }
-      ] 
+        { label: 'MacOS System8', action: () => window.open('https://sys8-board.vercel.app/', '_blank', 'noopener,noreferrer'), disabled: false }      ] 
     },
     { 
       name: '도움말', 
